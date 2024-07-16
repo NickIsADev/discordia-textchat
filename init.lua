9144d085620e804e8208935c879ebeb4a3c801d8
Troptop <87533146+NickIsADev@users.noreply.github.com>
2024-07-15 22:37:27 -0500

    Removed corresponding EventHandler removal

--- init.lua
+++ init.lua
@@ -1,10 +1,4 @@
 local discordia = require('discordia')
-local EventHandler = require('EventHandler')
-
-do
-    local client = discordia.Client({logFile = ''})
-    client._events.MESSAGE_CREATE = EventHandler.MESSAGE_CREATE
-end
 
 require('GuildVoiceChannel')
 
